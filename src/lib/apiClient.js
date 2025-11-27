b3ef40e221edde64a0ab1b2dbf60334cf90bba47
Okemaelvis <okemaelvis@gmail.com>
2025-11-27 19:53:37 +0300

    changes

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // ✅ uses Vercel environment variable
+  baseURL: "https://rehabserver.onrender.com", // ✅ uses Vercel environment variable
 });
 
 export default API;
